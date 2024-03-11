e1e36c2d379074d6ad3c601e5a47866835951439
BikramNarayan <lcb2022029@iiitl.ac.in>
2024-03-12 00:46:41 +0530

    Some minor changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -41,25 +41,21 @@ const Nav = () => {
   };
   return (
     <div
-      className={`w-full fixed top-0 left-0 bg-white transition-all duration-300 ${scrolled ? "shadow-md" : ""
-        }`}
+      className={`w-full fixed top-0 left-0 bg-white transition-all duration-300 ${
+        scrolled ? "shadow-md" : ""
+      }`}
       style={{ zIndex: 1000 }} // Increase the z-index value
       id="nav1"
     >
-      <div className="md:flex items-center justify-between py-4 md:px-10 px-7">
+      <div className="md:flex items-center justify-between py-4 md:px-10 px-7 ">
         <div
           className="font-bold text-3xl cursor-pointer flex items-center font-Poppins text-green-600" // Change the text color to green
         >
           <span className="text-white-3xl text-indigo-600 mr-1 pt-2 ">
             {/* <ion-icon name="cafe-outline"></ion-icon> */}
           </span>
-          <Link to="/">
-
-          Coffee Dates
-          </Link>
-
+          <Link to="/">Coffee Dates</Link>
         </div>
-
 
         <div
           onClick={() => setOpen(!open)}
@@ -70,8 +66,9 @@ const Nav = () => {
         </div>
 
         <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static text-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? "top-20 " : "top-[-490px]"
-            }`}
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static text-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? "top-20 " : "top-[-490px]"
+          }`}
         >
           {/* starting here */}
           <li
